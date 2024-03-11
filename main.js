dd8a5f622fb071cb9239e7748ed736f59b4acf6e
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2024-03-11 10:05:38 -0300

    akhuiow

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const botoes = document.querySelectorAll(".botao");
 for (let i= 0; i <botoes.length;i++){
     botoes[i].onclick = function(){
         for (let j=0; j <botoes.length;j++){
-            
+            botoes[j].classList.remove("ativo");
         }
+        botoes[i].add("ativo");
     }
 }
